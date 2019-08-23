b5f1475292d6b01b92733d86d4bfc8af3b799b18
ChaitanyaAkula <krishchaituakula@gmail.com>
2019-08-23 15:02:15 +0530

    Update dbconnection.go

--- dbconnection.go
+++ dbconnection.go
@@ -8,7 +8,7 @@ import(
 var db *sql.DB
 func Connection()*sql.DB{
 
-	Db,err := sql.Open("mysql","root:root@tcp(localhost:3306)/ancons")
+	Db,err := sql.Open("mysql","uscolleges:Ancons927@tcp(uscolleges.c96w2gvk26br.us-west-2.rds.amazonaws.com:3306)/uscolleges")
 	
 	if err!=nil{
 		log.Fatalln(err)
